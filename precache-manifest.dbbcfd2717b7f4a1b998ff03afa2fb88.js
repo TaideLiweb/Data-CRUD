7c5979caa1fc35d7fcd9f5123e531d2918a202c7
Ted <teddy55987@gmail.com>
2020-05-06 17:58:48 +0800

    Updates

---
+++ precache-manifest.dbbcfd2717b7f4a1b998ff03afa2fb88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d15076a9c173458eddce52ba54e58c7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6e59b44a3b0b5ca465f1",
+    "url": "./static/css/main.f39aad0a.chunk.css"
+  },
+  {
+    "revision": "8ced00507c75e1883bcd",
+    "url": "./static/js/2.69163372.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.69163372.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e59b44a3b0b5ca465f1",
+    "url": "./static/js/main.b95d06f8.chunk.js"
+  },
+  {
+    "revision": "d5782a4900331458817e",
+    "url": "./static/js/runtime-main.439f9fbd.js"
+  }
+]);
